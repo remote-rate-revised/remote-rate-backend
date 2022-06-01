d7b100f4b55880cedcb7426d04589c106ffb4b1e
Charlie Fadness <fadness.charlie@outlook.com>
2022-06-01 12:16:55 -0700

    added more cors options

--- server.js
+++ server.js
@@ -41,9 +41,17 @@ function getKey(header, callback) {
 // ---------------------------------------
 
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// Enable CORS
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
